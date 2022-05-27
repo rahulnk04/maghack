a8a70cb5253b6f69ca58a1f7b60fdc03544e33ac
Rahul Nayak <rahnayak@publicisgroupe.net>
2022-05-27 12:56:36 +0530

    Deploying test Red  App confirmation

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import React from "react";
 
 const App = () => {
-  return <div style={{ fontSize: 100 }}>MagHack</div>;
+  return <div style={{ fontSize: 100 }} className="text-danger">MagHack</div>;
 };
 
 export default App;
