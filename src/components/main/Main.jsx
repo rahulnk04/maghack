0412dbf2b13d985888a0ab53456515e4466eae6a
Rahul Nayak <rahnayak@publicisgroupe.net>
2022-05-30 14:49:52 +0530

    React Qstns done

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,118 @@
+import React, { Fragment } from "react";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { a11yDark as docco } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+const Main = ({ topics, qstns }) => {
+  return (
+    <Fragment>
+      <Stack
+        direction={{ xs: "column", sm: "column", md: "row" }}
+        spacing={{ xs: 1, sm: 1, md: 1 }}
+      >
+        {topics.map((r, i) => (
+          <Chip
+            color="info"
+            label={r.name}
+            id={r.Id}
+            key={i}
+            onClick={() => {
+              alert("comming");
+            }}
+          />
+        ))}
+        <Chip
+          label={`Total Question - ${qstns.length}`}
+          //   variant="outlined"
+          color="primary"
+          onClick={() => {
+            alert("comming");
+          }}
+        />
+      </Stack>
+      {qstns.map((r, i) => (
+        <Box key={i} sx={{ mt: 3 }}>
+          <Box sx={{ display: "flex" }}>
+            <Avatar
+              sx={{
+                width: 23,
+                height: 23,
+                bgcolor: "#FE414D",
+                fontSize: 10,
+                mr: 1,
+              }}
+            >
+              {i + 1}
+            </Avatar>
+            <Typography style={{ color: "#079FFF", fontSize: 16 }} variant="h6">
+              {r["question"]}
+            </Typography>
+          </Box>
+
+          <Box sx={{ color: "#000", mt: 1 }}>
+            <Typography variant="body2">{r["answer"]}</Typography>
+          </Box>
+          {r["bulletsTrue"] && (
+            <Box>
+              <ul className="text-muted">
+                {r["bullets"].map((b, bi) => (
+                  <li key={bi}>
+                    <Typography variant="body2">{b}</Typography>
+                  </li>
+                ))}
+              </ul>
+            </Box>
+          )}
+          {r["codeTrue"] && (
+            <Fragment>
+              {r["codes"].map((c, ci) => (
+                <Box key={ci}>
+                  <Box sx={{ color: "#FE414D" }}>
+                    <Typography variant="caption" sx={{ fontSize: 13 }}>
+                      {c["codeHead"]}
+                    </Typography>
+                  </Box>
+                  <SyntaxHighlighter
+                    lineProps={{
+                      style: {
+                        wordBreak: "break-all",
+                        whiteSpace: "pre-wrap",
+                        fontSize: 14,
+                      },
+                    }}
+                    wrapLines={true}
+                    language="jsx"
+                    style={docco}
+                  >
+                    {c["code"]}
+                  </SyntaxHighlighter>
+                </Box>
+              ))}
+            </Fragment>
+          )}
+          {r["imageTrue"] && (
+            <Fragment>
+              {r["images"].map((img, imi) => (
+                <Box key={imi}>
+                  <img
+                    className="img-fluid"
+                    src={img["image"]}
+                    alt={img["image"]}
+                  />
+                  <br />
+                  <span style={{ color: "#FE414D" }}>{img["imageHead"]}</span>
+                </Box>
+              ))}
+            </Fragment>
+          )}
+        </Box>
+      ))}
+    </Fragment>
+  );
+};
+
+export default Main;
--- src/components/main/react/ReactLearn.jsx
+++ src/components/main/react/ReactLearn.jsx
@@ -1,12 +1,7 @@
 import React, { Fragment, useEffect } from "react";
-import Chip from "@mui/material/Chip";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { a11yDark as docco } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { useSelector, useDispatch } from "react-redux";
 import { getReactTopics, getReactQstns } from "../../../redux/actions/actions";
+import Main from "../Main";
 
 const ReactLearn = () => {
   const dispatch = useDispatch();
@@ -26,103 +21,7 @@ const ReactLearn = () => {
   };
   return (
     <Fragment>
-      <Stack
-        direction={{ xs: "column", sm: "column", md: "row" }}
-        spacing={{ xs: 1, sm: 1, md: 1 }}
-      >
-        {reacttopics.map((r, i) => (
-          <Chip
-            color="info"
-            label={r.name}
-            id={r.Id}
-            key={i}
-            onClick={() => {
-              alert("comming");
-            }}
-          />
-        ))}
-        <Chip
-          label={`Total Question - ${reactqstns.length}`}
-          //   variant="outlined"
-          color="primary"
-          onClick={() => {
-            alert("comming");
-          }}
-        />
-      </Stack>
-      {reactqstns.map((r, i) => (
-        <Box key={i} sx={{ mt: 3 }}>
-          <Box sx={{ display: "flex" }}>
-            <Avatar
-              sx={{
-                width: 20,
-                height: 20,
-                bgcolor: "#FE414D",
-                fontSize: 14,
-                mr: 1,
-              }}
-            >
-              {i + 1}
-            </Avatar>
-            <h6 style={{ color: "#079FFF" }}>
-              {/* {"("}
-            {i + 1} */}
-
-              {/* {")"}- */}
-              {r["question"]}
-            </h6>
-          </Box>
-
-          <Box sx={{ color: "#000", mt: 1 }}>{r["answer"]}</Box>
-          {r["bulletsTrue"] && (
-            <div>
-              <ul className="text-muted">
-                {r["bullets"].map((b, bi) => (
-                  <li key={bi}>{b}</li>
-                ))}
-              </ul>
-            </div>
-          )}
-          {r["codeTrue"] && (
-            <div>
-              {r["codes"].map((c, ci) => (
-                <div key={ci}>
-                  <Box sx={{ color: "#FE414D" }}>{c["codeHead"]}</Box>
-                  <SyntaxHighlighter
-                    lineProps={{
-                      style: {
-                        wordBreak: "break-all",
-                        whiteSpace: "pre-wrap",
-                        fontSize: 14,
-                      },
-                    }}
-                    wrapLines={true}
-                    language="jsx"
-                    style={docco}
-                  >
-                    {c["code"]}
-                  </SyntaxHighlighter>
-                </div>
-              ))}
-            </div>
-          )}
-          {r["imageTrue"] && (
-            <div>
-              {r["images"].map((img, imi) => (
-                <div key={imi}>
-                  <img
-                    className="img-fluid"
-                    src={img["image"]}
-                    alt={img["image"]}
-                  />
-                  <br />
-                  <span style={{ color: "#FE414D" }}>{img["imageHead"]}</span>
-                </div>
-              ))}
-            </div>
-          )}
-        </Box>
-      ))}
+      <Main topics={reacttopics} qstns={reactqstns} />
     </Fragment>
   );
 };
