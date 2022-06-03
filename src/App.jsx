0a2fa74412c0eff1f7bd573fb829d98461ea0235
Rahul Nayak <rahnayak@publicisgroupe.net>
2022-06-03 14:01:15 +0530

    Under construction Page

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Home from "./components/home/Home";
 import "./App.css";
 import Wrapper from "./components/layout/Wrapper";
 import ReactLearn from "./components/main/react/ReactLearn";
-import AngularLearn from "./components/main/angular/AngularLearn";
 import ReactMain from "./components/main/react/ReactMain";
 import ReactMCQ from "./components/main/react/ReactMCQ";
-import ReactTutorial from "./components/main/react/ReactTutorial";
+// import ReactTutorial from "./components/main/react/ReactTutorial";
+
+// import AngularLearn from "./components/main/angular/AngularLearn";
+import UnderConstruction from "./components/layout/UnderConstruction";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -27,13 +29,17 @@ const App = () => {
             <Route exact path="/react" element={<ReactMain />} />
             <Route exact path="/react-questions" element={<ReactLearn />} />
             <Route exact path="/react-mcq" element={<ReactMCQ />} />
-            <Route exact path="/react-tutorial" element={<ReactTutorial />} />
+            {/* <Route exact path="/react-tutorial" element={<ReactTutorial />} /> */}
 
-            <Route exact path="/angular" element={<AngularLearn />} />
+            {/* <Route exact path="/angular" element={<AngularLearn />} /> */}
             <Route
               exact
               path="/*"
-              element={<div>Site Under construction</div>}
+              element={
+                <div>
+                  <UnderConstruction />
+                </div>
+              }
             />
           </Route>
 
